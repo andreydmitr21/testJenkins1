d10102508cca7ff76896508cb25b4f911c199d8e
andreydmitr21 <andreydmitr21@gmail.com>
2022-05-27 14:05:10 +0200

    simple tests

---
+++ src/test/java/tests/simple/SkippedTests.java
@@ -0,0 +1,31 @@
+package tests.simple;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SkippedTests {
+
+    @Test
+    @Disabled
+    void test00() {
+        assertTrue(false);
+
+    }
+
+    @Test
+    @Disabled
+    void test01() {
+        assertTrue(false);
+
+    }
+
+    @Test
+    @Disabled("some reason")
+    void test02() {
+        assertTrue(false);
+
+    }
+
+}
