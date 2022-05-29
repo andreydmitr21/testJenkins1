8dfeed45216e5611076d415b8dc09e8733c65e17
andreydmitr21 <andreydmitr21@gmail.com>
2022-05-29 23:02:13 +0200

    added SelenideTests

---
+++ src/test/java/tests/demoqa/SelenideTests.java
@@ -0,0 +1,48 @@
+package tests.demoqa;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
+
+@Tag("demoqa")
+public class SelenideTests {
+
+    @Test
+    void shouldFindSelenideInGithub() {
+        Configuration.baseUrl = " https://github.com";
+        Configuration.browserSize = "1920x1080";
+        // open
+        step("Open", () -> {
+            open("/");
+        });
+
+        step("Search", () -> {
+            $("[data-test-selector=nav-search-input]")
+                    .setValue("selenide")
+                    .pressEnter();
+        });
+
+        // find and click on selenide repo
+        step("Select repo", () -> {
+            $$(".repo-list li")
+                    .first()
+                    .$("a")
+                    .click();
+        });
+        //$$(".v-align-middle").findBy(text("selenide/selenide")).click();
+        // check if real selenide repo url
+        step("Check ", () -> {
+            $("[rel=author]")
+                    .shouldHave(text("selenide"));
+        });
+        step("Check 1", () -> {
+            $("[itemprop=name]")
+                    .shouldHave(text("selenide"));
+        });
+        //$("h1").shouldHave(text("selenide / selenide"));
+    }
+}
