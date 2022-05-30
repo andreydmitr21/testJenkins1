2d654f0957aada402ada9d6ecab7d49e5212c470
andreydmitr21 <andreydmitr21@gmail.com>
2022-05-30 11:56:38 +0200

    added Attach

--- src/test/java/tests/demoqa/SelenideTests.java
+++ src/test/java/tests/demoqa/SelenideTests.java
@@ -1,6 +1,7 @@
 package tests.demoqa;
 
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
@@ -11,6 +12,7 @@ import static io.qameta.allure.Allure.step;
 @Tag("demoqa")
 public class SelenideTests extends TestBase{
 
+    @DisplayName("Github test")
     @Test
     void shouldFindSelenideInGithub() {
 
--- src/test/java/tests/demoqa/TestBase.java
+++ src/test/java/tests/demoqa/TestBase.java
@@ -1,6 +1,10 @@
 package tests.demoqa;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestBase {
@@ -10,5 +14,15 @@ public class TestBase {
         Configuration.baseUrl = " https://github.com";
         Configuration.browserSize = "1920x1080";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
     }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+    }
+
 }
