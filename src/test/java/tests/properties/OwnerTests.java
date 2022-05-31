7945dbd40e018c0e8ba2435375ae62843b7c8d0d
andreydmitr21 <andreydmitr21@gmail.com>
2022-05-31 17:22:18 +0200

    added credntial.properties and OwnerTests

---
+++ src/test/java/tests/properties/OwnerTests.java
@@ -0,0 +1,15 @@
+package tests.properties;
+
+import org.junit.jupiter.api.Test;
+
+public class OwnerTests {
+
+    @Test
+    void loginTest() {
+        String login = "";
+        String password = "";
+        System.out.println("Login: " + login);
+        System.out.println("Pass: " + password);
+
+    }
+}
