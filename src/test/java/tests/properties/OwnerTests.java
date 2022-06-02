f91ab2bdd4b286cca9a97aefa5302a1202a16187
andreydmitr21 <andreydmitr21@gmail.com>
2022-06-02 16:14:45 +0200

    added owner.example

--- src/test/java/tests/properties/OwnerTests.java
+++ src/test/java/tests/properties/OwnerTests.java
@@ -1,16 +1,30 @@
 package tests.properties;
 
+import com.codeborne.selenide.Selenide;
+import config.CredentialsConfig;
+import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 public class OwnerTests {
 
+
+    CredentialsConfig config =
+            ConfigFactory.create(CredentialsConfig.class);
+
     @Test
+    @Tag("owner")
     void loginTest() {
-        String login = "";
-        String password = "";
+
+
+        String login = config.login();
+        String password = config.password();
 
         System.out.println("Login: " + login);
         System.out.println("Pass: " + password);
 
+        String mess = "I log as " + login + " " + password;
+        System.out.println(mess);
     }
 }
